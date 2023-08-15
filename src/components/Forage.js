881203e2c5bafc9d1f3bb218d279eb4c471809ec
joshuaenoch <joshuaechow@gmail.com>
2023-08-15 10:51:42 -0700

    forage update

--- src/components/Forage.js
+++ src/components/Forage.js
@@ -1,10 +1,25 @@
 import React from "react"
 import '../style.css';
-import plants from './PlantList';
-import { Carousel } from "react-responsive-carousel";
+//import plants from './PlantList';
+//import { Carousel } from "react-responsive-carousel";
 
 export default function Forage() {
     return (
+      <div className = "forage-container">
+        <div className = "forage-intro">
+          <p>To see which plants can be foraged from what trail, check out the <a href="/list">Trails</a> page</p>
+        </div>
+        <div className = "forage">
+          <iframe className = "forage-iframe"
+            src={`https://docs.google.com/document/d/${'1dxmMAglPLiya88FfAD_nO33pzG50dkgRvc4nsfqPYFY/edit?usp=sharing'}/pub?embedded=true&rm=minimal`}
+            title="Embedded Google Doc"
+          />
+        </div>
+      </div>
+
+    );
+
+    /*return (
         <div className = "forage">
           <ul>
                 {plants.map((plant, index) => (
@@ -30,5 +45,5 @@ export default function Forage() {
                 ))}
             </ul>
         </div>
-    );
+    );*/
 }
